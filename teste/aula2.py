d89caa949812295361356c4b83dfff1b3b4babd9
Carlos Magno <carlosmagnopz@gmail.com>
2025-05-03 15:58:57 -0300

    agora foi

---
+++ teste/aula2.py
@@ -0,0 +1,5 @@
+valor = float(input('Valor do item: '))
+quantidade = int(input('Quantidade de itens: '))
+total = valor * quantidade
+desconto = total*0.9
+print (f'o valor total deu: R$ {desconto:.2f}') 
