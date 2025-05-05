6086294bb723b25bb271fa6c9162eee465d46052
Carlos Magno <carlosmagnopz@gmail.com>
2025-05-05 18:43:56 -0300

    Aula 5

---
+++ teste/string.py
@@ -0,0 +1,27 @@
+
+
+
+
+#Concatenação(+):
+ #'olá ' + 'mundo!' -> 'olá mundo!'
+palavra_1 = 'olá'
+#palavra_2 = 'mundo'
+#print(palavra_1 + palavra_2)
+
+#Repetição(* ): 
+ #3 * 'w' ou 'w' * 3 -> 'www'
+#print(palavra_1*3)
+
+#Comparação:
+ #Operadores relacionais: == !=, >, >=, <, <=
+print('a' < 'b')
+print('a' > 'b')
+print('a' == 'b')
+print('a' != 'b')
+print(ord('a')) #ord pode ser usado para verificar o codigo da letra
+print('aaa' < 'aaa')
+print('aaa' <= 'aaa')
+print('aaa' < 'aab')
+
+#formatação
+print(f'texto formatado: {palavra_1}')
